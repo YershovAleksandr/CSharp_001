2ad1b258c15357fef039c6e5583c99a47c23741b
hp <namstudionsk@gmail.com>
2018-01-08 03:00:57 +0700

    Add all files

---
+++ Console1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Console1
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			Console.WriteLine ("Hello World!");
+			Console.WriteLine ("ZZ = " + System.Environment.OSVersion.Platform.ToString());
+
+		}
+	}
+}
